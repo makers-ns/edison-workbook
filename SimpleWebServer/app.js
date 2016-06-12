f028118177987364d44fea9b3ed070e47c293cef
Zoran <sprintrz@gmail.com>
2016-06-12 15:37:29 +0200

    fix path

--- SimpleWebServer/app.js
+++ SimpleWebServer/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('files'));
+app.use(express.static(__dirname+'/files'));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
